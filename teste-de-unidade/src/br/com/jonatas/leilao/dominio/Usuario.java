4d4d445ce70e0e2b35e2524fa30493ecf9887f7b
Jônatas Santos <jonatas.santos@aedu.com>
2019-09-09 16:47:34 -0300

    Refatorado a classe Usuario para utilizacao da biblioteca lombok

--- teste-de-unidade/src/br/com/jonatas/leilao/dominio/Usuario.java
+++ teste-de-unidade/src/br/com/jonatas/leilao/dominio/Usuario.java
@@ -1,5 +1,8 @@
 package br.com.jonatas.leilao.dominio;
 
+import lombok.Data;
+
+@Data
 public class Usuario {
 
 	private int id;
@@ -13,42 +16,4 @@ public class Usuario {
 		this.id = id;
 		this.nome = nome;
 	}
-
-	public int getId() {
-		return id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Usuario other = (Usuario) obj;
-		if (id != other.id)
-			return false;
-		if (nome == null) {
-			if (other.nome != null)
-				return false;
-		} else if (!nome.equals(other.nome))
-			return false;
-		return true;
-	}
-
-	
 }
