7d7e381009c63ebc2ae7b369f8789167770e7dfa
Jônatas Santos <jonatas.santos@aedu.com>
2019-09-10 10:42:51 -0300

    Refatorado a classe de teste encerrador de leilao para acessar a lista do Dao.

--- teste-de-unidade/test/br/com/jonatas/leilao/servico/EncerradorDeLeilaoTeste.java
+++ teste-de-unidade/test/br/com/jonatas/leilao/servico/EncerradorDeLeilaoTeste.java
@@ -2,10 +2,12 @@ package br.com.jonatas.leilao.servico;
 
 import br.com.jonatas.leilao.builder.CriadorDeLeilao;
 import br.com.jonatas.leilao.dominio.Leilao;
+import br.com.jonatas.leilao.infra.dao.LeilaoDao;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Calendar;
+import java.util.List;
 
 public class EncerradorDeLeilaoTeste {
     @Test
@@ -16,11 +18,18 @@ public class EncerradorDeLeilaoTeste {
         Leilao leilao1 = new CriadorDeLeilao().para("TV de plasma").naData(antiga).constroi();
         Leilao leilao2 = new CriadorDeLeilao().para("Fogao elétrico").naData(antiga).constroi();
 
+        LeilaoDao dao = new LeilaoDao();
+        dao.salva(leilao1);
+        dao.salva(leilao2);
+
         EncerradorDeLeilao encerrador = new EncerradorDeLeilao();
         encerrador.encerra();
 
-        Assert.assertTrue(leilao1.isEncerrado());
-        Assert.assertTrue(leilao2.isEncerrado());
+        List<Leilao> encerrados = dao.encerrados();
+
+        Assert.assertEquals(2, encerrados.size());
+        Assert.assertTrue(encerrados.get(0).isEncerrado());
+        Assert.assertTrue(encerrados.get(1).isEncerrado());
     }
 
 }
