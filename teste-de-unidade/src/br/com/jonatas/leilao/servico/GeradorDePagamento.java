8cc2bf560e2ffefd6379d1d2173d697cbe858da2
Jônatas Santos <jonatas.santos@aedu.com>
2019-09-12 16:37:23 -0300

    Regatorado o construtor da classe GeradorDePagamento.

--- teste-de-unidade/src/br/com/jonatas/leilao/servico/GeradorDePagamento.java
+++ teste-de-unidade/src/br/com/jonatas/leilao/servico/GeradorDePagamento.java
@@ -5,6 +5,7 @@ import br.com.jonatas.leilao.dominio.Pagamento;
 import br.com.jonatas.leilao.infra.dao.LeilaoDao;
 import br.com.jonatas.leilao.infra.dao.PagamentosDao;
 import br.com.jonatas.leilao.infra.relogio.Relogio;
+import br.com.jonatas.leilao.infra.relogio.RelogioDoSistema;
 
 import java.util.Calendar;
 import java.util.List;
@@ -13,11 +14,17 @@ public class GeradorDePagamento {
     private final LeilaoDao leiloes;
     private final Avaliador avaliador;
     private final PagamentosDao pagamentos;
+    private final Relogio relogio;
 
     public GeradorDePagamento(LeilaoDao leiloes, PagamentosDao pagamentos, Avaliador avaliador, Relogio relogio) {
         this.leiloes = leiloes;
         this.avaliador = avaliador;
         this.pagamentos = pagamentos;
+        this.relogio = relogio;
+    }
+
+    public GeradorDePagamento(LeilaoDao leiloes, PagamentosDao pagamentos, Avaliador avaliador) {
+        this(leiloes, pagamentos, avaliador, new RelogioDoSistema());
     }
 
     public void gera(){
